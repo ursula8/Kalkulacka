04c5ebddf801429981851e21d96d5e01d7586d8c
ursula8 <ursula.chladecka@gmail.com>
2020-09-16 19:48:59 +0200

    Pridanie cyklov na prechádzanie zadaných čísiel-násobiteľov

--- src/com/company/StringMultiplication.java
+++ src/com/company/StringMultiplication.java
@@ -9,7 +9,25 @@ public class StringMultiplication {
 
         cislo1=icislo1;
         cislo2=icislo2;
+        int lengt1=cislo1.length();
+        int lengt2=cislo2.length();
+        int nasobok=0;
+        int kurzor=0;
+        int medzivysledok[]=new int[lengt1+lengt2];  //kazdy vypocitany medzivysledok sa uklada na inú poziciu v poli
 
-        System.out.println("Zaznamenali sme čísla: "+cislo1+cislo2);
+        for(int i=lengt1-1;i>=0;i--){                   //prechadza prve cislo z pravej strany az po prvu cislicu
+           int nasobitel1=cislo1.charAt(i)-'0';         // i-nasobitel prveho cisla
+           System.out.println("Nasobitel1"+nasobitel1);
+
+            for(int j=lengt2-1;j>=0;j--){               //prechadze druhe cislo
+                int nasobitel2=cislo2.charAt(i)-'0';         // i-nasobitel druheho cisla
+                System.out.println("Nasobitel2"+nasobitel2);
+                nasobok=nasobitel1*nasobitel2;          //vynasobenie nasobitela i s nasobitelom j
+                int zvysok=nasobok/10;                  //po súčine ukladá prenášanu desiatku
+                medzivysledok[kurzor]=nasobok%10;       //po súčine ukladá jednotku výsledku na novej
+            }
+        }
+
+        System.out.println("Zaznamenali sme čísla: "+nasobok);
     }
 }
