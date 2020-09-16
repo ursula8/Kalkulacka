3e794a49d13e96729780be4a0828cb27f2491b9e
ursula8 <ursula.chladecka@gmail.com>
2020-09-16 18:44:12 +0200

    Rišenie algoritmu BigInteger

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("\n---------------------------------------\n" +
+                "Vitaj v programe jednoduchá kalkulačka!" +
+                "\n---------------------------------------\n" +
+                "- Zadaj čísla,ktoré si želáš vynásobiť -\n");
+
+        Scanner citacka = new Scanner(System.in);
+
+        System.out.print("Prvé čislo:");
+        BigInteger prveCislo = citacka.nextBigInteger();
+
+        System.out.print("Druhé čislo:");
+        BigInteger druheCislo = citacka.nextBigInteger();
+
+        BigInteger vysledok = prveCislo.multiply(druheCislo);
+        System.out.println("Výsledok je: " + vysledok);
+
     }
 }
